981c967a68b1da9500bc3a6a02fd56a52061fc02
Mathias <mathias.castro@utec.edu.pe>
2025-09-14 17:50:16 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,33 @@
-variable "region"           { type = string  default = "us-east-1" }
-variable "image_uri"        { type = string  description = "ECR Image URI" }
-variable "lab_role_arn"     { type = string  description = "arn:aws:iam::<ACCOUNT_ID>:role/LabRole" }
-variable "container_port"   { type = number  default = 8000 }
-variable "desired_count"    { type = number  default = 1 }
-variable "health_check_path"{ type = string  default = "/students" }
+variable "region" {
+  description = "AWS region (Academy usa us-east-1)"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "image_uri" {
+  description = "ECR Image URI (ej: 123456789012.dkr.ecr.us-east-1.amazonaws.com/api-students:latest)"
+  type        = string
+}
+
+variable "lab_role_arn" {
+  description = "ARN del rol existente LabRole (arn:aws:iam::<ACCOUNT_ID>:role/LabRole)"
+  type        = string
+}
+
+variable "container_port" {
+  description = "Puerto en el que escucha tu app"
+  type        = number
+  default     = 8000
+}
+
+variable "desired_count" {
+  description = "Cantidad de tareas en el servicio"
+  type        = number
+  default     = 1
+}
+
+variable "health_check_path" {
+  description = "Ruta del health check del ALB Target Group"
+  type        = string
+  default     = "/students"
+}
