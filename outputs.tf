25bfbb51e2e1f4b5ee5d38fc25cfe6e522caa3f9
Mathias <mathias.castro@utec.edu.pe>
2025-09-14 17:50:04 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
-output "alb_dns_name"  { value = aws_lb.this.dns_name  description = "URL público del ALB" }
-output "cluster_name"  { value = aws_ecs_cluster.this.name }
-output "service_name"  { value = aws_ecs_service.this.name }
+output "alb_dns_name" {
+  description = "URL público del ALB"
+  value       = aws_lb.this.dns_name
+}
+
+output "cluster_name" {
+  value = aws_ecs_cluster.this.name
+}
+
+output "service_name" {
+  value = aws_ecs_service.this.name
+}
