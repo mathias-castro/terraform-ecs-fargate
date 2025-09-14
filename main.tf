d60a5f8338dc414fbbff906d0856349fe85a0364
Mathias <mathias.castro@utec.edu.pe>
2025-09-14 18:04:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,47 @@
 provider "aws" { region = var.region }
 
-# Security Groups
+# ---------- Security Groups ----------
 resource "aws_security_group" "alb" {
   name        = "alb-sg"
   description = "ALB SG"
   vpc_id      = var.vpc_id
-  ingress { from_port=80 to_port=80 protocol="tcp" cidr_blocks=["0.0.0.0/0"] }
-  egress  { from_port=0 to_port=0 protocol="-1" cidr_blocks=["0.0.0.0/0"] }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "svc" {
   name        = "svc-sg"
   description = "Service SG"
   vpc_id      = var.vpc_id
+
   ingress {
     from_port       = var.container_port
     to_port         = var.container_port
     protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
   }
-  egress { from_port=0 to_port=0 protocol="-1" cidr_blocks=["0.0.0.0/0"] }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-# ALB + TG + Listener
+# ---------- ALB ----------
 resource "aws_lb" "this" {
   name               = "api-students-alb"
   load_balancer_type = "application"
@@ -37,6 +56,7 @@ resource "aws_lb_target_group" "this" {
   target_type = "ip"
   port        = var.container_port
   protocol    = "HTTP"
+
   health_check {
     path                = var.health_check_path
     matcher             = "200-399"
@@ -51,16 +71,20 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
   protocol          = "HTTP"
-  default_action { type = "forward" target_group_arn = aws_lb_target_group.this.arn }
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
 }
 
-# Logs
+# ---------- Logs ----------
 resource "aws_cloudwatch_log_group" "this" {
   name              = "/ecs/api-students-tf"
   retention_in_days = 7
 }
 
-# ECS
+# ---------- ECS ----------
 resource "aws_ecs_cluster" "this" {
   name = "api-students-tf-cluster"
 }
@@ -73,11 +97,15 @@ resource "aws_ecs_task_definition" "this" {
   memory                   = "512"
   execution_role_arn       = var.lab_role_arn
   task_role_arn            = var.lab_role_arn
+
   container_definitions = jsonencode([{
     name      = "api-students"
     image     = var.image_uri
     essential = true
-    portMappings = [{ containerPort = var.container_port, protocol = "tcp" }]
+    portMappings = [{
+      containerPort = var.container_port
+      protocol      = "tcp"
+    }]
     logConfiguration = {
       logDriver = "awslogs"
       options = {
